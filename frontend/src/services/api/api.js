11946bf2033eee7dfa67c80368d25a3df057c51d
Bruno Galvão <bgalvaomiranda302@gmail.com>
2026-02-22 13:14:43 -0300

    chore: configure environment variables for API base URL and update path resolution in Vite config

--- frontend/src/services/api/api.js
+++ frontend/src/services/api/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: `${BASE_URL}/v1`,
   headers: {
     "Content-Type": "application/json",
   },
